7b50875512775947b722ed748b073a4dca44fe9d
spacearound <viksnamax@mail.ru>
2022-01-07 13:36:51 +0300

    Add send request method

--- client_payment_sdk/client.py
+++ client_payment_sdk/client.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
+import decimal
+
+import requests
+
 
 class ClientPaymentSDK(object):
     """
@@ -10,9 +14,18 @@ class ClientPaymentSDK(object):
         self._public_id = public_id
         self._api_secret = api_secret
 
-    def init(self, param):
+    def _send_request(self, endpoint, params=None, request_id=None):
+        headers = None
+        if request_id is not None:
+            headers = {'X-Request-ID': request_id}
+
+        response = requests.post(self.URL + endpoint, json=params, headers=headers)
+
+        return response.json(parse_float=decimal.Decimal)
+
+    def init(self, params):
         """
-        Any Payment Initiation
+        Payment Initiation
 
         # Arguments
 
@@ -21,5 +34,9 @@ class ClientPaymentSDK(object):
         # Returns
 
         """
-        return
+        endpoint = '/init'
+
+        response = self._send_request(endpoint, params)
+
+        return response
 
