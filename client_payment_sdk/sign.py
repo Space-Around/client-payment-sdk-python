6471668172ff0d55055a27062337ee2396aed9c5
spacearound <viksnamax@mail.ru>
2022-01-10 01:11:18 +0300

    Add different method

--- client_payment_sdk/sign.py
+++ client_payment_sdk/sign.py
@@ -3,14 +3,19 @@ from jwcrypto.common import json_encode, json_decode
 from .utils import dict_to_str
 
 
-def sign(endpoint, payload, secret):
+def sign(endpoint, method, payload, secret):
     header = {'alg': 'HS256'}
 
     key = jwk.JWK.from_password(secret)
 
     sorted_param = dict(sorted(payload.items(), key=lambda x: x[0]))
 
-    payload_dict = {'PATH': endpoint, 'POST': sorted_param}
+    if method == 'POST':
+        payload_dict = {'PATH': endpoint, 'POST': sorted_param}
+    else:
+        # TODO: add raise if not method get
+        payload_dict = {'PATH': endpoint, 'GET': sorted_param}
+
     payload_str = dict_to_str(payload_dict)
 
     jwstoken = jws.JWS(payload_str.encode('utf-8'))
