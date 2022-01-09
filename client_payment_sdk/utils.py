f053b125dc5d72759b6af26a36d192c110aac251
spacearound <viksnamax@mail.ru>
2022-01-09 19:40:55 +0300

    Add function dict to str

--- client_payment_sdk/utils.py
+++ client_payment_sdk/utils.py
@@ -2,18 +2,10 @@ from jwcrypto import jws, jwk
 from jwcrypto.common import json_encode, json_decode
 
 
-def sign(endpoint, payload, secret):
-    header = {'alg': 'HS256'}
+def dict_to_str(data_dict):
+    data_str = ''
 
-    key = jwk.JWK.from_password(secret)
+    for key in data_dict:
+        data_str += f'"{key}":{dict_to_str(data_dict[key])},' if isinstance(data_dict[key], dict) else f'"{key}":"{data_dict[key]}",'
 
-    sorted_param = dict(sorted(payload.items(), key=lambda x: x[0]))
-
-    payload_dict = {'PATH': endpoint, 'GET': sorted_param}
-    payload_str = str(payload_dict).replace('\'', '"').replace(' ', '')
-
-    jwstoken = jws.JWS(payload_str.encode('utf-8'))
-    jwstoken.add_signature(key, None, json_encode(header), json_encode({"kid": key.thumbprint()}))
-    jws_data = json_decode(jwstoken.serialize())
-
-    return jws_data['signature']
+    return '{' + data_str[0:len(data_str)-1] + '}'
