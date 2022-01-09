2e05a9cf271f72ba19129c9ea167a16398e481c0
spacearound <viksnamax@mail.ru>
2022-01-09 16:24:57 +0300

    +

--- examples/payment.py
+++ examples/payment.py
@@ -1,13 +1,18 @@
 from client_payment_sdk import ClientPaymentSDK
+from client_payment_sdk import sign
 
 
 api_secret = 'aa21444f3f71'
 params = {
-    'amount': '10',
-    'order': 'order-00001',
+    'amount': '100',
     'merchant_id': '13',
+    'order': 'fb11ff6c-7145-11ec-9e37-0242ac130018',
     'product_id': '15'
 }
 
+params['signature'] = sign('/init', params, api_secret)
+
 client = ClientPaymentSDK(None, api_secret)
-print(client.init(params))
+html = client.init(params)
+
+print(html)
