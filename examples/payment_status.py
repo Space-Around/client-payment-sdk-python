febd4c24abf368658eb24667acb8d1753d3fd4ff
spacearound <viksnamax@mail.ru>
2022-01-10 01:11:46 +0300

    Add payment status example

---
+++ examples/payment_status.py
@@ -0,0 +1,17 @@
+from client_payment_sdk import ClientPaymentSDK, sign
+
+
+api_secret = 'aa21444f3f71'
+
+params = {
+    'merchant_id': '13',
+    'order': 'fb11ff6c-7145-11ec-9e37-0242ac130032',
+    'product_id': '15'
+}
+
+params['signature'] = sign('/status', 'GET', params, api_secret)
+
+client = ClientPaymentSDK(None, api_secret)
+result = client.status(params)
+
+print(result)
