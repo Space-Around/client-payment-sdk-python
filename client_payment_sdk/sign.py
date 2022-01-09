384fc80d48043e984aeedc5742ff3a995121f14b
spacearound <viksnamax@mail.ru>
2022-01-09 19:45:25 +0300

    Fixed convert dict to string in payload

--- client_payment_sdk/sign.py
+++ client_payment_sdk/sign.py
@@ -1,5 +1,6 @@
 from jwcrypto import jws, jwk
 from jwcrypto.common import json_encode, json_decode
+from .utils import dict_to_str
 
 
 def sign(endpoint, payload, secret):
@@ -9,8 +10,8 @@ def sign(endpoint, payload, secret):
 
     sorted_param = dict(sorted(payload.items(), key=lambda x: x[0]))
 
-    payload_dict = {'PATH': endpoint, 'GET': sorted_param}
-    payload_str = str(payload_dict).replace('\'', '"').replace(' ', '')
+    payload_dict = {'PATH': endpoint, 'POST': sorted_param}
+    payload_str = dict_to_str(payload_dict)
 
     jwstoken = jws.JWS(payload_str.encode('utf-8'))
     jwstoken.add_signature(key, None, json_encode(header), json_encode({"kid": key.thumbprint()}))
