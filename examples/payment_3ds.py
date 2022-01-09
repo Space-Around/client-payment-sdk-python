cfa07bb18413c0adcc7360ee1f9b0e96d82786a3
spacearound <viksnamax@mail.ru>
2022-01-09 20:06:53 +0300

    Add example for 3ds payment init

---
+++ examples/payment_3ds.py
@@ -0,0 +1,28 @@
+from client_payment_sdk import ClientPaymentSDK, sign
+
+
+api_secret = 'aa21444f3f71'
+
+params = {'amount': '100',
+          'merchant_id': '13',
+          'order': 'fb11ff6c-7145-11ec-9e37-0242ac130021',
+          'product_id': '15',
+          'device_channel': 'BRW',
+          'device_browser_ip': '0.0.0.0',
+          'device_browser_accept_header': 'text/html',
+          'device_browser_java_enabled': 'false',
+          'device_browser_language': 'RU',
+          'device_browser_color_depth': '32',
+          'device_browser_screen_height': '800',
+          'device_browser_screen_width': '480',
+          'device_browser_tz': '60, 120, -180',
+          'device_browser_user_agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0',
+          'challenge_window_size': '02'
+          }
+
+params['signature'] = sign('/init', params, api_secret)
+
+client = ClientPaymentSDK(None, api_secret)
+result = client.init(params)
+
+print(result)
