31879e42c26fbf7c36c7c31d14288b65f4306e40
spacearound <viksnamax@mail.ru>
2022-01-14 16:34:08 +0300

    +

---
+++ examples/payment_sbp.py
@@ -0,0 +1,17 @@
+from client_payment_sdk import ClientPaymentSDK, sign
+
+
+api_secret = 'aa21444f3f71'
+params = {
+    'amount': 10,
+    'merchant_id': 15,
+    'order': '241eeda4-7491-11ec-9c7f-0242ac130021',
+    'product_id': 17,
+    'notification_url': 'http://25ab-91-241-13-31.ngrok.io/webhooks'
+}
+
+params['signature'] = sign('/init', 'POST', params, api_secret)
+client = ClientPaymentSDK()
+result = client.init(params)
+
+print(f'url: {result.url}, payment_redirect_url: {result.payment_redirect_url}')
