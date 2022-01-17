c4ca6d74e56810cd6519544df1f4db53855f7aba
spacearound <viksnamax@mail.ru>
2022-01-17 19:37:08 +0300

    +

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ setup(
     packages=['client_payment_sdk'],
 
     description='Client Payment Python SDK',
+    long_description_content_type='text/markdown',
     long_description=long_description,
 
     version=VERSION,
@@ -39,21 +40,10 @@ setup(
     ],
 
     classifiers=[
-        'Development Status :: 1 - Production/Stable',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Topic :: Office/Business',
-        'Topic :: Office/Business :: Financial',
+        'Programming Language :: Python :: 3.10',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ],
 
